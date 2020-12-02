518920038e9bb6eeba10e72ccc6a864a3a0e4317
vchandwani <varunchandwani@gmail.com>
2020-12-02 16:27:19 +1100

    Databse entry for user

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
-require('./models/Users');
 
 //Connect Mongodb
 mongoose.connect(keys.mongoURI);
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy(
     {
@@ -10,12 +12,13 @@ passport.use(new GoogleStrategy(
     callbackURL: '/auth/google/callback '
     },
     (accessToken, refreshToken, profile, done)=>{
-      console.log('accessToken'); 
-      console.log(accessToken); 
-      console.log('done ' + done); 
-      console.log('profile'); 
-      console.log(profile); 
-      console.log('refreshToken'); 
-      console.log(refreshToken); 
+      User.findOne({googleId : profile.id})
+      .then((existingUser)=>{
+        if(existingUser){
+
+        } else {
+          new User({googleId : profile.id}).save();
+        }
+      })
     }
 ));
