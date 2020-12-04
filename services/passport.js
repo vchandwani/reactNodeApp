6bb333c7ad0bad78167e587092946060636d6901
vchandwani <varunchandwani@gmail.com>
2020-12-04 16:50:56 +1100

    Correction for param
    Correction for param

--- services/passport.js
+++ services/passport.js
@@ -10,31 +10,29 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then((user) => {
+  User.findById(id).then(user => {
     done(null, user);
   });
 });
 
-passport.use(
-  new GoogleStrategy(
+passport.use(new GoogleStrategy(
     {
-      clientID: keys.googleClientID,
+      clientID : keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-      proxy: true,
+      callbackURL: '/auth/google/callback ',
+      proxy:true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          // we already have a record with the given profile ID
-          done(null, existingUser);
+    (accessToken, refreshToken, profile, done)=>{
+      User.findOne({googleId : profile.id})
+      .then((existingUser)=>{
+        if(existingUser){
+          // user already exists
+          done(null,existingUser);
         } else {
-          // we don't have a user record with this ID, make a new record!
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
+          //new user
+          new User({googleId : profile.id}).save()
+          .then(user => done(null,user));
         }
-      });
+      })
     }
-  )
-);
+));
