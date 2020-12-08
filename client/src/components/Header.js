e5a2e120315bb8f82904341b89c89d07c4997425
varunchandwani <varunchandwani@gmail.com>
2020-12-08 22:15:52 +1100

    Landing Component added, Auth Router changes for redirect

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 class Header extends Component {
+  
+  renderContent () {
+    switch(this.props.auth) {
+      case null :
+        return;
+      case false:
+        return (
+          <li><a href='/auth/google'>Login with Google</a></li>
+        );
+      default:
+        return (
+          <li><a href='/api/logout'>Logout</a></li>
+        );
+    }
+  }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">
+          <Link to={this.props.auth ? '/surveys': '/' } className="left brand-logo">
             Emaily
-          </a>
+          </Link>
           <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +34,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps(state){
+  return { auth : state.auth} ;
+}
+ 
+export default connect(mapStateToProps)(Header);
